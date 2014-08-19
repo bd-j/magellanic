5e5bbb272e7cc59aecdf6a89028bade059a726eb
Benjamin Johnson <benjamin.duncan.johnson@gmail.com>
2014-08-18 18:26:38 -0700

    region to pixel mapping, and streamlined prediction code

---
+++ sfhs/mc_regutils.py
@@ -0,0 +1,58 @@
+import numpy as np
+import sfhutils as utils
+
+def xy_to_regname(x,y, cloud='smc'):
+    """
+    Map image pixels to region names
+    """
+    if cloud.lower() == 'smc':
+        factor = 1
+    elif cloud.lower() == 'lmc':
+        factor = 2
+    a = ord('A')
+    reg =  chr(a+x/factor), chr(a+y/factor), x % factor, y % factor
+    if factor == 2:
+        return '{0}{1}_{2}{3}'.format(*reg)
+    else:
+        return '{0}{1}'.format(*reg[0:2])
+
+def regname_to_xy(regname, cloud='smc'):
+    """
+    Map region names to image pixels
+    """
+    n = regname
+    a = ord('A')
+    x, y = ord(n[0]) - a, ord(n[1]) - a 
+    if cloud.lower() == 'smc':
+        #done
+        return (x,) , (y,)
+    elif cloud.lower() == 'lmc':
+        if len(n) > 2:
+            #there is a subgrid designation
+            sx, sy = int(n[3]), int(n[4])
+            return (x*2 + sx,) , (y*2 + sy,)
+        else:
+            # there is no subgrid designation and we return the x,y of
+            # all four subgrid pixels
+            x = x*2 + np.array([0,0,1,1])
+            y = y*2 + np.array([0,1,0,1])
+            return tuple(x), tuple(y)
+        
+def mc_ast(cloud):
+    """
+    Set up the sky coordinate system.
+    """
+    if cloud.lower() == 'lmc':
+        regions = utils.lmc_regions()
+        dm = 18.5
+        cdelt = [24./60./np.cos(np.deg2rad(-69.38333)),  24./60.]
+        crpix = [ 0, 0]
+        crval = [ 67.75 - cdelt[0]/2, -72.3833]
+    elif cloud.lower() == 'smc':
+        regions = utils.smc_regions()
+        #the minimum ra in deg of the 'A' corner
+        crval = [6.25, -74.95]
+        crpix = [0., 0.]
+        #the dra in deg worked out from figure 3 of H & Z
+        cdelt = [0.5/11 * 15., 12./60.]
+    return crpix, crval, cdelt, regions
--- sfhs/predicted_image.py
+++ sfhs/predicted_image.py
@@ -3,147 +3,138 @@ import sys
 import numpy as np
 import matplotlib.pyplot as pl
 import pickle
-
 import astropy.io.fits as pyfits
-import regionsed
 import fsps
 
-# Methods for reading H&Z data as dictionaries
-#  of structured arrays
+import regionsed
+import mc_regutils as regutils
 import sfhutils as utils
 
-cloud, filters = 'smc', ['galex_NUV', 'spitzer_irac_ch1', 'spitzer_irac_ch4', 'spitzer_mips_24']
-min_tpagb_age, lf_band, wave, agb_dust = 0.0, '4', '{8\mu m}', 1.0
-ldir = 'lf_data/'
-outdir = 'tmp/'
-#########
-# Initialize the import objects (SPS, SFHs, LFs)
-#########
-# SPS
-sps = fsps.StellarPopulation(add_agb_dust_model = True)
-sps.params['agb_dust'] = agb_dust
-dust = ['nodust', 'agbdust']
-sps.params['imf_type'] = 0.0
-
-# SFHs
-if cloud.lower() == 'lmc':
-    regions = utils.lmc_regions()
-    dm = 18.5
-    zlist = [7, 11, 13, 16]
-    lffiles = ['{0}z{1:02.0f}_tau{2:02.0f}_vega_irac{3}_lf.txt'.format(ldir, z, agb_dust*10, lf_band) for z in zlist]
-elif cloud.lower() == 'smc':
-    regions = utils.smc_regions()
-    dm = 18.9
-    zlist = [7, 13, 16]
-    lffiles = ['{0}z{1:02.0f}_tau{2:02.0f}_vega_irac{3}_lf.txt'.format(ldir, z, agb_dust*10, lf_band) for z in zlist]
-else:
-    print('do not understand your MC designation')
-
-# LFs
-try:
-    lf_bases = [utils.read_lfs(f) for f in lffiles]
-    #zero out select ages
-
-    for j, base in enumerate(lf_bases):
-        blank = base['ssp_ages'] <= min_tpagb_age
-        base['lf'][blank,:] = 0
+def main():
+    cloud, filters = 'smc', ['galex_NUV', 'spitzer_irac_ch1', 'spitzer_irac_ch4', 'spitzer_mips_24']
+    min_tpagb_age, lf_band, wave, agb_dust = 0.0, '4', '{8\mu m}', 0.5
+    ldir, outdir = 'lf_data/', 'test/'
     
-        #plot the lfs to make sure they are ok
-        ncolors = base['lf'].shape[0]
-        cm = pl.get_cmap('gist_rainbow')
-        fig = pl.figure()
-        ax = fig.add_subplot(111)
-        ax.set_color_cycle([cm(1.*i/ncolors) for i in range(ncolors)])
-        for i,t in enumerate(base['ssp_ages']):
-            ax.plot(base['bins'], base['lf'][i,:], linewidth = 3,
-                    label = '{:4.2f}'.format(t), color = cm(1.*i/ncolors))
-        ax.legend(loc =0, prop = {'size':6})
-        ax.set_ylim(1e-6,3e-4)
-        ax.set_yscale('log')
-        ax.set_xlabel(r'$M_{}$'.format(wave))
-        ax.set_ylabel(r'$n(<M, t)$')
-        fig.savefig('{}.png'.format(lffiles[j].replace('.txt','')))
-        pl.close(fig)
+    #########
+    # Initialize the import objects (SPS, SFHs, LFs)
+    #########
+    # SPS
+    sps = fsps.StellarPopulation(add_agb_dust_model = True)
+    sps.params['agb_dust'] = agb_dust
+    dust = ['nodust', 'agbdust']
+    sps.params['imf_type'] = 0.0
 
-except(NameError):
-    lf_bases = None
-
-    
-###############
-# Main piece of code to do all the SFH integrations
-###############
-dat = regionsed.regionsed(regions, sps, lf_bases = lf_bases, filters = filters,)
-locs, name, mags, lfs = dat
-#bins = lf_basis['bins']
-bins = regionsed.lbins
-ind_lmax = np.searchsorted(bins, -8.5) -1
-
-#############
-# Build output images and LF cubes
-############
-# Convert pixel letters to numbers and generate output
-cell = [( ord(n[0].upper()) - ord('A'), ord(n[1].upper()) - ord('A')) for n in name]
-cell = np.array(cell)
-# Dilate lmc pixels
-factor = 2 - int(cloud == 'smc')
-nx, ny = (cell[:,0].max()+1), (cell[:,1].max()+1)
-im = np.zeros([ len(filters), nx * factor, ny * factor])
-agb = np.zeros([ len(bins), nx * factor, ny * factor])
-
-for i, n in enumerate(name):
-    x, y = cell[i,:] * factor
-    if (len(n) > 2) or (factor == 1):
-        if len(n) > 2:
-            sx, sy = int(n[3]), int(n[4])
-        else:
-            sx, sy = 0, 0
-        im[:, x+sx, y+sy] = 10**(-0.4 * mags[i,:])
-        if lfs is not None:
-            agb[:, x+sx, y+sy] = lfs[i,:]
+    # SFHs
+    if cloud.lower() == 'lmc':
+        regions = utils.lmc_regions()
+        nx, ny, dm = 48, 38, 18.5
+        zlist = [7, 11, 13, 16]
+        lffiles = ['{0}z{1:02.0f}_tau{2:02.0f}_vega_irac{3}_lf.txt'.format(ldir, z, agb_dust*10, lf_band) for z in zlist]
+    elif cloud.lower() == 'smc':
+        regions = utils.smc_regions()
+        nx, ny, dm = 20, 23, 18.9
+        zlist = [7, 13, 16]
+        lffiles = ['{0}z{1:02.0f}_tau{2:02.0f}_vega_irac{3}_lf.txt'.format(ldir, z, agb_dust*10, lf_band) for z in zlist]
     else:
-        im[:, x:x+2, y:y+2] = 10**(-0.4 * mags[i,:, None, None])/4.
-        if lfs is not None:
-            agb[:, x:x+2, y:y+2] = lfs[i,:, None, None]/4.
+        print('do not understand your MC designation')
 
-#############
-# Write output
-############
-            
-# Write out images as fits and jpg
-for i, f in enumerate(filters):
+    # LFs
+    try:
+        lf_bases = [utils.read_lfs(f) for f in lffiles]
+        #zero out select ages
+        for j, base in enumerate(lf_bases):
+            blank = base['ssp_ages'] <= min_tpagb_age
+            base['lf'][blank,:] = 0
+            plot_lf(base, wave, lffiles[j])
+
+    except(NameError):
+        lf_bases = None
+
+    ###############
+    # Main piece of code to do all the SFH integrations
+    ###############
+
+    dat = regionsed.regionsed(regions, sps, lf_bases = lf_bases, filters = filters,)
+    locs, name, mags, lfs = dat
+    bins = regionsed.lbins
+    ind_lmax = np.searchsorted(bins, -8.5) -1
+
+    #############
+    # Build output images and LF cubes
+    ############
+
+    im = np.zeros([ len(filters), nx, ny])
+    agb = np.zeros([ len(bins), nx, ny])
+    for i, n in enumerate(name):
+        x, y = regutils.regname_to_xy(n, cloud=cloud)
+        agb[:, x, y] = (lfs[i, :, None] / np.size(x))
+        im[:, x, y] = (10**(-0.4 * mags[i,:, None])/ np.size(x))
+
+    #############
+    # Write output
+    ############
+    for i, f in enumerate(filters):
+        write_image(im[i,:,:].T, cloud, f, outdir=outdir, agb_dust=agb_dust)
+    
+    #write out AGB N(>M) images as fits
+    #for lim in np.arange(-6.7, -9.5, -0.5):
+    #    ind = np.searchsorted(bins, lim) -1
+    #    pyfits.writeto('test.fits', agb[ind,:,:].T, clobber = True)
+
+    #write out AGB N(>M) images as a pickle file
+    agb_cube = {}
+    agb_cube['agb_clf_cube'] = agb
+    agb_cube['mag_bins'] = bins
+    out = open("{0}clf.{1}.tau{2:02.0f}.irac{3}.p".format(outdir, cloud.lower(), agb_dust*10, lf_band), "wb")
+    pickle.dump(agb_cube, out)
+    out.close()
+
+    # Plot the total LF
     fig, ax = pl.subplots(1,1)
-    image = ax.imshow(np.log10(im[i,:,:].T), interpolation = 'nearest', origin = 'lower')
-    ax.set_title('{0}  @ {1}'.format(cloud.upper(), f))
+    lf_tot = agb.sum(-1).sum(-1)
+    ax.plot(bins + dm, lf_tot)
+    ax.set_ylabel(r'$N(<M)$ (total for cloud)')
+    ax.set_xlabel(r'$m_{}$ (Vega apparent)'.format(wave))
+    ax.set_title(cloud.upper())
+    ax.set_yscale('log')
+    fig.savefig('{0}total_agb_clf.{1}.tau{2:02.0f}.irac{3}.png'.format(outdir, cloud.lower(), agb_dust*10, lf_band))
+
+def write_image(im, cloud, filtname, outdir='./', agb_dust=1.0):
+    """
+    Write out images as fits and jpg
+    """
+    fig, ax = pl.subplots(1,1)
+    image = ax.imshow(np.log10(im), interpolation = 'nearest', origin = 'lower')
+    ax.set_title('{0}  @ {1}'.format(cloud.upper(), filtname))
     ax.set_xlabel('RA (pixels)')
     ax.set_ylabel('Dec (pixels)')
     cbar = pl.colorbar(image, orientation = 'horizontal', shrink = 0.7, pad = 0.12)
-    cbar.set_label(r'log F({0}) (AB maggies)'.format(f))
-    fstring = '{3}{0}.log_{1}.{2}.harris_zaritsky'.format(cloud, f, dust[int(sps.params['agb_dust'])], outdir)
+    cbar.set_label(r'log F({0}) (AB maggies)'.format(filtname))
+    fstring = '{0}hzpred.{1}.tau{2:02.0f}.{3}'.format( outdir, cloud, agb_dust*10, filtname)
     pl.savefig(fstring + '.png')
     pl.close(fig)
-    pyfits.writeto(fstring + '.fits', im[i,:,:].T, clobber = True)
+    pyfits.writeto(fstring + '.fits', im, clobber = True)
 
-#write out AGB N(>M) images as fits
-for lim in np.arange(-6.7, -9.5, -0.5):
-    ind = np.searchsorted(bins, lim) -1
-#    pyfits.writeto('test.fits', agb[ind,:,:].T, clobber = True)
-
-#write out AGB N(>M) images as a pickle file
-agb_cube = {}
-agb_cube['agb_clf_cube'] = agb
-agb_cube['mag_bins'] = bins
-out = open("{0}clf.{1}.tau{02:2.0f}.irac{3}.p".format(outdir, cloud.lower(), agb_dust*10, lf_band), "wb")
-pickle.dump(agb_cube, out)
-out.close()
-
-# Plot the total LF
-fig, ax = pl.subplots(1,1)
-lf_tot = agb.sum(-1).sum(-1)
-ax.plot(bins + dm, lf_tot)
-ax.set_ylabel(r'$N(<M)$ (total for cloud)')
-ax.set_xlabel(r'$m_{}$ (Vega apparent)'.format(wave))
-ax.set_title(cloud.upper())
-ax.set_yscale('log')
-fig.savefig('{0}total_agb_clf.{1}.tau{02:2.0f}.irac{3}.png'.format(outdir, cloud.lower(), agb_dust*10, lf_band))
+def plot_lf(base, wave, lffile):
+    """
+    Plot the interpolated input lfs to make sure they are ok
+    """
+    ncolors = base['lf'].shape[0]
+    cm = pl.get_cmap('gist_rainbow')
+    fig = pl.figure()
+    ax = fig.add_subplot(111)
+    ax.set_color_cycle([cm(1.*i/ncolors) for i in range(ncolors)])
+    for i,t in enumerate(base['ssp_ages']):
+        ax.plot(base['bins'], base['lf'][i,:], linewidth = 3,
+                label = '{:4.2f}'.format(t), color = cm(1.*i/ncolors))
+    ax.legend(loc =0, prop = {'size':6})
+    ax.set_ylim(1e-6,3e-4)
+    ax.set_yscale('log')
+    ax.set_xlabel(r'$M_{}$'.format(wave))
+    ax.set_ylabel(r'$n(<M, t)$')
+    fig.savefig('{}.png'.format(lffile.replace('.txt','')))
+    pl.close(fig)
 
 
+if __name__ == '__main__':
+    main()
